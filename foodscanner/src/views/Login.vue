53492d8fd4cd60ec07b15a357eddd965952db049
Juanjo López <juanjoselopez@usal.es>
2022-05-21 12:02:38 +0200

    Setting up

---
+++ foodscanner/src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+    <div class="col-sm-1">
+      <img src="../assets/foodStock.jpg" width="1000"/>
+    </div>
+    <div class="col-sm-6">
+     
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'LoginView',
+  components: {
+
+  }
+}
+</script>
